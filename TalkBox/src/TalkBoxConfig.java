2ec8153ae363d7292dde733b3f03b77067f7efbd
Eric Pham <Eric@DESKTOP-CVTRRP1.mshome.net>
2019-01-14 22:58:49 -0500

    Created the two classes for the two apps and started the GUI

---
+++ TalkBox/src/TalkBoxConfig.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+
+public class TalkBoxConfig {
+	
+	public JPanel panel;
+	
+	public JButton num1;
+	public JButton num2;
+	public JButton num3;
+	public JButton num4;
+	public JButton num5;
+	public JButton num6;
+	public JButton num7;
+	public JButton num8;
+	public JButton num9;
+	public JButton num0;
+	
+	public JFrame frame;
+	
+	public TalkBoxConfig(){
+		frame = new JFrame("TalkBox");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(1000, 500);
+		
+		panel = new JPanel();
+		panel.setLayout(null);
+		
+		num1 = new JButton("1");
+		num2 = new JButton("2");
+		num3 = new JButton("3");
+		num4 = new JButton("4");
+		num5 = new JButton("5");
+		num6 = new JButton("6");
+		num7 = new JButton("7");
+		num8 = new JButton("8");
+		num9 = new JButton("9");
+		num0 = new JButton("0");
+	
+		num1.setBounds(0,100,75,75);
+		num2.setBounds(75,100,75,75);
+		num3.setBounds(150,100,75,75);
+		num4.setBounds(0,175,75,75);
+		num5.setBounds(75,175,75,75);
+		num6.setBounds(150,175,75,75);
+		num7.setBounds(0,250,75,75);
+		num8.setBounds(75,250,75,75);
+		num9.setBounds(150,250,75,75);
+		num0.setBounds(0,325,150,75);
+		
+		panel.add(num1);
+		panel.add(num2);
+		panel.add(num3);
+		panel.add(num4);
+		panel.add(num5);
+		panel.add(num6);
+		panel.add(num7);
+		panel.add(num8);
+		panel.add(num9);
+		panel.add(num0);
+		
+		frame.add(panel);
+		
+	}
+}
---
+++ TalkBox/src/TalkBoxSim.java
@@ -0,0 +1,12 @@
+
+public class TalkBoxSim {
+	
+	public static void main(String [] args) {
+		new TalkBoxSim();
+	}
+	
+	public TalkBoxSim() {
+		TalkBoxConfig config = new TalkBoxConfig();
+	}
+
+}
