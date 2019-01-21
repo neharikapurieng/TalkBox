76aee564a394cfb7a04055d82deede418a72d42b
Eric Pham <Eric@DESKTOP-CVTRRP1.mshome.net>
2019-01-21 13:50:53 -0500

    Created packages for the configuration and the sim

--- TalkBox/src/TalkBoxSim/Gui.java
+++ TalkBox/src/TalkBoxSim/Gui.java
@@ -1,3 +1,4 @@
+package TalkBoxSim;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
--- TalkBox/src/TalkBoxSim/Main.java
+++ TalkBox/src/TalkBoxSim/Main.java
@@ -1,3 +1,4 @@
+package TalkBoxSim;
 
 	
 import javafx.application.Application;
