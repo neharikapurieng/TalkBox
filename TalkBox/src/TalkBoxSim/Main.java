8cb91b7eb9429cf3998d47c675cdee0687c2eed9
Neharika Puri <neharikapurieng@gmail.com>
2019-02-18 13:32:56 -0500

    scene, layout, show

--- TalkBox/src/TalkBoxSim/Main.java
+++ TalkBox/src/TalkBoxSim/Main.java
@@ -14,10 +14,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,400,400); // Scene (layout)
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();
+			primaryStage.show(); // just displays it to the user
 		} catch(Exception e) {
 			e.printStackTrace();
 		 
