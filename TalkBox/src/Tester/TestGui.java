d997ac19fbba51686aeb4146dc92b8ff05e0b318
Eric <erictpham22@gmail.com>
2019-02-19 05:25:21 -0500

    Tester

---
+++ TalkBox/src/Tester/TestGui.java
@@ -0,0 +1,21 @@
+package Tester;
+
+import TalkBoxConfig.GuiConfig;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestGui extends Application {
+
+	public GuiConfig g;
+	@Override
+	public void start(Stage arg0) throws Exception {
+		g = new GuiConfig();
+		g.start(arg0);
+	}
+
+	public static void main(String [] args) {
+		Application.launch(args);
+	}
+	
+	
+}
