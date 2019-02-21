e19b23fb60dced4551185b7b98bb76a6c5d23069
Yonis <yabokar@gmail.com>
2019-02-21 13:33:54 -0500

    Added a FileManager Tester to Test the Finder Method which is now implemented in the FileManager Class

---
+++ TalkBox/src/Tester/TestFileManager.java
@@ -0,0 +1,61 @@
+package Tester;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import TalkBoxConfig.FileManager;
+
+class FileManagerTest {
+
+	@Test
+	void testFindFilesReturnArrayofFiles() throws IOException {
+		
+		FileManager file  =  new FileManager();
+		Object[] files = file.finder("waves");
+		assertEquals(File[].class,files.getClass());              
+	}
+	
+	@Test
+	void testFindFilesNonExistantDir() {
+		
+		try {
+			
+		
+		FileManager file  =  new FileManager();
+		Object[] files = file.finder("hello");
+		assertFalse(true,"Expected exception to be thrown");
+		
+		}
+		catch(IOException e) {
+			
+			assertTrue(true);
+		}
+          
+	}
+	
+	
+	
+	@Test
+	void testReturnsEmptyFileArrayIfPathIsNull() throws IOException {
+		
+		FileManager file  =  new FileManager();
+		Object[] files = file.finder(null);
+		assertEquals(0,files.length);  
+	}
+	
+	
+	@Test
+	void testReturnsallWavsInDirl() throws IOException {
+		
+		FileManager file  =  new FileManager();
+		Object[] files = file.finder("waves");
+		assertEquals(1,files.length);  
+	}
+
+
+	
+}
